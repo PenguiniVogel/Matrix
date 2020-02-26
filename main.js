38a68c44a663d8a307b3bdf001e31262a9982b25
Felix Vogel <20129564+FelixVogel@users.noreply.github.com>
2020-02-26 16:09:34 +0100

    Inital Commit

---
+++ main.js
@@ -0,0 +1,87 @@
+function graphics() {
+    var interval = 250;
+
+    var canvas = document.getElementById('matrix-canvas');
+    var graphics = document.getElementById('matrix-canvas').getContext('2d');
+
+    window.addEventListener('resize', resize);
+
+    function resize(e) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    resize();
+
+    graphics.scale(1.2, 1.2);
+
+    var chars = 'ｦｱｳｴｵｶｷｹｺｻｼｽｾｿﾀﾂﾃﾅﾆﾇﾈﾊﾋﾎﾏﾐﾑﾒﾓﾔﾕﾗﾘﾜ日(+*;)-|2589Z';
+
+    for (var x = 1; x < window.innerWidth; x += 12) {
+        function create(x) {
+            var column = [createItem(x)];
+
+            setTimeout(function () {
+                setInterval(function () {
+                    if(update(column)) {
+                        column.push(createItem(x));
+                    }
+
+                    column = column.filter(function (item) {
+                        return item.eraseY <= window.innerHeight;
+                    });
+                }, interval);
+            }, Math.floor(Math.random() * 2000) + 1000);
+        }
+
+        create(x);
+    }
+
+    function update(column) {
+        var needsNew = false;
+
+        for (var i = 0; i < column.length; i++) {
+            var item = column[i];
+
+            if (item.delay <= 0) {
+                graphics.fillStyle = '#44ff00';
+                graphics.fillText(chars[Math.floor(Math.random() * chars.length)], item.x, item.letterY);
+
+                item.letterY += 12;
+
+                if (item.letters >= item.max && !item.erasing) {
+                    item.erasing = true;
+
+                    needsNew = true;
+                } else {
+                    item.letters += 1;
+                }
+
+                if (item.erasing) {
+                    graphics.fillStyle = '#000000';
+                    graphics.fillRect(item.x, item.eraseY, 12, 12);
+
+                    item.eraseY += 12;
+                }
+            } else {
+                item.delay -= interval;
+            }
+        }
+
+        return needsNew;
+    }
+
+    function createItem(x) {
+        return {
+            x      : x,
+            letters: 0,
+            max    : Math.floor(Math.random() * 8) + 2,
+            letterY: 10,
+            erasing: false,
+            eraseY : 0,
+            delay  : Math.floor(Math.random() * 8000) + 2000
+        };
+    }
+}
+
+setTimeout(function () {graphics();}, 100);
