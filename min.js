585f1fa8b2e794eb1d944997d733bd7d088e72c0
Felix Vogel <20129564+FelixVogel@users.noreply.github.com>
2020-02-27 13:58:05 +0100

    TypeScript

--- min.js
+++ min.js
@@ -1,24 +1,24 @@
 /**
-MIT License
+ MIT License
 
-Copyright (c) 2020 Felix Vogel
+ Copyright (c) 2020 Felix Vogel
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-function graphics(){var a=250;var t=document.getElementById("matrix-canvas");var i=document.getElementById("matrix-canvas").getContext("2d");window.addEventListener("resize",e);function e(e){t.width=window.innerWidth;t.height=window.innerHeight}e();i.scale(1.2,1.2);var l="ｦｱｳｴｵｶｷｹｺｻｼｽｾｿﾀﾂﾃﾅﾆﾇﾈﾊﾋﾎﾏﾐﾑﾒﾓﾔﾕﾗﾘﾜ日(+*;)-|2589Z";for(var n=1;n<window.innerWidth;n+=12){if(n+12>window.innerWidth){break}for(var r=0;r<window.innerHeight;r+=12){s(n,r)}o(n)}function o(e){var t=[h(e)];setTimeout(function(){setInterval(function(){if(f(t)){t.push(h(e))}i.globalAlpha=.005;d(0,0,window.innerWidth,window.innerHeight);i.globalAlpha=1;t=t.filter(function(e){return e.eraseY<=window.innerHeight})},a)},Math.floor(Math.random()*2e3)+1e3)}function f(e){var t=false;for(var n=0;n<e.length;n++){var r=e[n];if(r.delay<=0){d(r.x,r.letterY,12,12);i.strokeStyle="#44ff00";i.strokeText(l[Math.floor(Math.random()*l.length)],r.x+1,r.letterY);r.letterY+=12;if(r.letters>=r.max&&!r.erasing){r.erasing=true;t=true}else{r.letters+=1}if(r.erasing){d(r.x,r.eraseY,12,12);s(r.x,r.eraseY);r.eraseY+=12}}else{r.delay-=a}}return t}function h(e){return{x:e,letters:0,max:Math.floor(Math.random()*8)+2,letterY:10,erasing:false,eraseY:0,delay:Math.floor(Math.random()*8e3)+2e3}}function d(e,t,n,r){i.fillStyle="#000000";i.fillRect(e,t,n,r)}function s(e,t){i.fillStyle="#44ff00";i.globalAlpha=Math.max(.05,Math.min(.5,Math.random()));i.fillRect(e+2,t+4,2,2);i.globalAlpha=1}}setTimeout(function(){graphics()},100);
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+function graphics(){var a=250;var t=document.getElementById("matrix-canvas");var i=t.getContext("2d");var e=function(e){t.width=window.innerWidth;t.height=window.innerHeight};e(null);window.addEventListener("resize",e);i.scale(1.2,1.2);var l=function(e,t,r,n){i.fillStyle="#000000";i.fillRect(e,t,r,n)};var o=function(e,t){i.fillStyle="#44ff00";i.globalAlpha=Math.max(.05,Math.min(.5,Math.random()));i.fillRect(e+2,t+4,2,2);i.globalAlpha=1};var f="ｦｱｳｴｵｶｷｹｺｻｼｽｾｿﾀﾂﾃﾅﾆﾇﾈﾊﾋﾎﾏﾐﾑﾒﾓﾔﾕﾗﾘﾜ日(+*;)-|2589Z";var r=function(e){var t=[d(e)];setTimeout(function(){return setInterval(function(){if(s(t)){t.push(d(e))}i.globalAlpha=.005;l(0,0,window.innerWidth,window.innerHeight);i.globalAlpha=1;t=t.filter(function(e){return e.eraseY<=window.innerHeight})},a)},Math.floor(Math.random()*2e3)+1e3)};for(var n=1;n<window.innerWidth;n+=12){if(n+12>window.innerWidth)break;for(var h=0;h<window.innerHeight;h+=12)o(n,h);r(n)}var s=function(e){var t=false;for(var r=0;r<e.length;r++){var n=e[r];if(n.delay<=0){l(n.x,n.letterY,12,12);i.strokeStyle="#44ff00";i.strokeText(f[Math.floor(Math.random()*f.length)],n.x+1,n.letterY);n.letterY+=12;if(n.letters>=n.max&&!n.erasing){n.erasing=true;t=true}else{n.letters+=1}if(n.erasing){l(n.x,n.eraseY,12,12);o(n.x,n.eraseY);n.eraseY+=12}}else{n.delay-=a}}return t};var d=function(e){return{x:e,letters:0,max:Math.floor(Math.random()*8)+2,letterY:10,erasing:false,eraseY:0,delay:Math.floor(Math.random()*8e3)+2e3}}}setTimeout(function(){return graphics()},100);
---
+++ script.ts
@@ -0,0 +1,138 @@
+/**
+ MIT License
+
+ Copyright (c) 2020 Felix Vogel
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+function graphics() {
+    const interval: number = 250;
+
+    const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('matrix-canvas');
+    const graphics: CanvasRenderingContext2D = canvas.getContext('2d');
+
+    const resize = (e: Event) => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    };
+
+    resize(null);
+    window.addEventListener('resize', resize);
+
+    graphics.scale(1.2, 1.2);
+
+    const paintRect = (x: number, y: number, width: number, height: number) => {
+        graphics.fillStyle = '#000000';
+        graphics.fillRect(x, y, width, height);
+    };
+
+    const paintDot = (x: number, y: number) => {
+        graphics.fillStyle = '#44ff00';
+        graphics.globalAlpha = Math.max(0.05, Math.min(0.5, Math.random()));
+        graphics.fillRect(x + 2, y + 4, 2, 2);
+        graphics.globalAlpha = 1;
+    };
+
+    const chars: String = 'ｦｱｳｴｵｶｷｹｺｻｼｽｾｿﾀﾂﾃﾅﾆﾇﾈﾊﾋﾎﾏﾐﾑﾒﾓﾔﾕﾗﾘﾜ日(+*;)-|2589Z';
+
+    const create = (x) => {
+        let column: IColumn[] = [createItem(x)];
+
+        setTimeout(() => setInterval(() => {
+            if(update(column)) {
+                column.push(createItem(x));
+            }
+
+            graphics.globalAlpha = 0.005;
+            paintRect(0, 0, window.innerWidth, window.innerHeight);
+            graphics.globalAlpha = 1;
+
+            column = column.filter(item => item.eraseY <= window.innerHeight);
+        }, interval), Math.floor(Math.random() * 2000) + 1000);
+    };
+
+    for (let x: number = 1; x < window.innerWidth; x += 12) {
+        if (x + 12 > window.innerWidth) break;
+
+        for (let y: number = 0; y < window.innerHeight; y += 12) paintDot(x, y);
+
+        create(x);
+    }
+
+    const update = (column: IColumn[]) => {
+        let needsNew: boolean = false;
+
+        for (let i: number = 0; i < column.length; i++) {
+            let item: IColumn = column[i];
+
+            if (item.delay <= 0) {
+                paintRect(item.x, item.letterY, 12, 12);
+
+                graphics.strokeStyle = '#44ff00';
+                graphics.strokeText(chars[Math.floor(Math.random() * chars.length)], item.x + 1, item.letterY);
+
+                item.letterY += 12;
+
+                if (item.letters >= item.max && !item.erasing) {
+                    item.erasing = true;
+
+                    needsNew = true;
+                } else {
+                    item.letters += 1;
+                }
+
+                if (item.erasing) {
+                    paintRect(item.x, item.eraseY, 12, 12);
+                    paintDot(item.x, item.eraseY);
+
+                    item.eraseY += 12;
+                }
+            } else {
+                item.delay -= interval;
+            }
+        }
+
+        return needsNew;
+    };
+
+    interface IColumn {
+        x       : number,
+        letters : number,
+        max     : number,
+        letterY : number,
+        erasing : boolean,
+        eraseY  : number,
+        delay   : number
+    }
+
+    const createItem = (x: number): IColumn => {
+        return {
+            x      : x,
+            letters: 0,
+            max    : Math.floor(Math.random() * 8) + 2,
+            letterY: 10,
+            erasing: false,
+            eraseY : 0,
+            delay  : Math.floor(Math.random() * 8000) + 2000
+        };
+    };
+}
+
+setTimeout(() => graphics(), 100);
